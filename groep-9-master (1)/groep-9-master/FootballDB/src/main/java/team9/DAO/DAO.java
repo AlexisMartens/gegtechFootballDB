ce3229910d2daffb868f011a28b6512d44be6868
Stijn Debyser <stijn.debyser@ugent.be>
2019-12-08 14:43:40 +0100

    DAO voor Club geimplementeerd

---
+++ groep-9-master (1)/groep-9-master/FootballDB/src/main/java/team9/DAO/DAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9.DAO;
+
+
+import team9.FootballDB.Club;
+import team9.Repositories.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author User
+ */
+public class DAO implements IDAO {
+    @Autowired
+    private ClubRepository clubRepository;
+    
+    @Override
+    public synchronized boolean addClub(Club club){
+        clubRepository.save(club);
+        return false;
+    }
+    
+    @Override
+    public List<Club> getAllClubs() {
+        List<Club> list = new ArrayList<>();
+        clubRepository.findAll().forEach(list::add);
+        return list;
+    }
+    
+    @Override
+    public Club getClubById(Integer clubId){
+        if(clubRepository.findById(clubId).isPresent()){
+            return clubRepository.findById(clubId).get();
+        }
+        else {
+            return null;
+        }
+    }
+    
+    public void updateClub(Club club){
+        clubRepository.save(club);
+    }
+    
+    public void deleteClub(Integer clubId){
+        clubRepository.delete(getClubById(clubId));
+    }
+}
---
+++ groep-9-master (1)/groep-9-master/FootballDB/src/test/java/team9/FootballDB/FootballDbApplicationTests.java
@@ -0,0 +1,13 @@
+package team9.FootballDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
