673f6a7a3f2956bf5bb2b553547b311d3d16258b
Stijn Debyser <stijn.debyser@ugent.be>
2019-12-08 21:54:19 +0100

    Geen idee

---
+++ FootballDB/src/main/java/team9/FootballDB/Persoon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9.FootballDB;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+/**
+ *
+ * @author User
+ */
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Persoon implements Serializable {
+    
+}
