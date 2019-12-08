13078ac334624d5f017e5a9346004f933ba9a254
Stijn Debyser <stijn.debyser@ugent.be>
2019-12-08 15:37:12 +0100

    Stadion repository

---
+++ FootballDB/src/main/java/team9/Repositories/StadionRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import team9.FootballDB.Stadion;
+
+/**
+ *
+ * @author User
+ */
+public interface StadionRepository extends CrudRepository<Stadion,Integer> {
+    
+}
