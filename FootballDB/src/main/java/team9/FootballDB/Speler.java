cb4bacfb22866360bf79d71ba66ff70857da3b3c
Stijn Debyser <stijn.debyser@ugent.be>
2019-12-08 21:17:39 +0100

    Klasse trainer aangemaakt

--- FootballDB/src/main/java/team9/FootballDB/Speler.java
+++ FootballDB/src/main/java/team9/FootballDB/Speler.java
@@ -5,10 +5,53 @@
  */
 package team9.FootballDB;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
 /**
  *
  * @author User
  */
+@Entity
+@Table(name = "spelers")
 public class Speler {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "speler_id")
+    private Integer spelerID;
+    
+    @Basic
+    @Column(name = "speler_name")
+    private String spelerName;
+    
+    // school is een Foreign-Key van tabel 'spelers' en verwijst naar een 'club'
+    @ManyToOne(targetEntity = Club.class)
+    @JoinColumn(name = "club_fk_speler", nullable = false)
+    private Club club;
+
+    public Integer getSpelerID() {
+        return spelerID;
+    }
+
+    public String getSpelerName() {
+        return spelerName;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setSpelerID(Integer spelerID) {
+        this.spelerID = spelerID;
+    }
+
+    public void setSpelerName(String spelerName) {
+        this.spelerName = spelerName;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
     
 }
---
+++ FootballDB/src/main/java/team9/FootballDB/Trainer.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team9.FootballDB;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+/**
+ *
+ * @author User
+ */
+@Entity
+@Table(name = "trainer")
+public class Trainer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "trainer_id")
+    private Integer trainerID;
+    
+    @Basic
+    @Column(name = "trainer_name")
+    private String trainerName;
+    
+    @OneToOne(targetEntity = Club.class)
+    @JoinColumn(name = "club_fk_trainer", nullable = false)
+    private Club club;
+
+    public Integer getTrainerID() {
+        return trainerID;
+    }
+
+    public String getTrainerName() {
+        return trainerName;
+    }
+
+    public Club getClub() {
+        return club;
+    }
+
+    public void setTrainerID(Integer trainerID) {
+        this.trainerID = trainerID;
+    }
+
+    public void setTrainerName(String trainerName) {
+        this.trainerName = trainerName;
+    }
+
+    public void setClub(Club club) {
+        this.club = club;
+    }
+    
+}
